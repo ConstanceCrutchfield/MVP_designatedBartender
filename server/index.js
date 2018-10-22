21067b1862ae67dc4c103dbd9b4c3ec097ad234d
Constance Crutchfield <cgcrutch18@gmail.com>
2018-10-22 17:00:40 -0500

    New app to heroku

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
-// const items = require('../database-mysql');
+const items = require('../database-mysql');
 // const items = require('../database-mongo');
 
 const app = express();
@@ -14,6 +14,28 @@ const app = express();
 app.use(express.static(path.join(__dirname, '/../angular-client')));
 app.use(express.static(path.join(__dirname, '/../node_modules')));
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`listening on port ${PORT}!`);
+});
+
+app.get('/', (req, res) => {
+
+});
+
+app.post('/create', (req, res) => {
+
+});
+
+app.get('/search', (req, res) => {
+
+});
+
+app.post('/update', (req, res) => {
+
+});
+
 app.get('/items', (req, res) => {
   items.selectAll((err, data) => {
     if (err) {
@@ -24,6 +46,4 @@ app.get('/items', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('listening on port 3000!');
-});
+
