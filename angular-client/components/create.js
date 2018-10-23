f92ffd08102abe810bf20dcdf0eb08e981a2b8e8
Constance Crutchfield <cgcrutch18@gmail.com>
2018-10-23 13:53:31 -0500

    All components rendered to page

---
+++ angular-client/components/create.js
@@ -0,0 +1,10 @@
+angular.module('app')
+  .component('create', {
+    bindings: {
+      items: '<',
+    },
+    controller() {
+
+    },
+    templateUrl: '/templates/create.html',
+  });
---
+++ angular-client/components/search.js
@@ -0,0 +1,10 @@
+angular.module('app')
+  .component('search', {
+    bindings: {
+      items: '<',
+    },
+    controller() {
+
+    },
+    templateUrl: '/templates/search.html',
+  });
